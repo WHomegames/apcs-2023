0c4d01dab9d4a34adad1645ed51af6f220c5ebcf
DonnerNW School Acc <61599222+WHomegames@users.noreply.github.com>
2023-03-23 20:37:29 +0000

    modified:   unit10/Recur.java

--- unit10/Recur.java
+++ unit10/Recur.java
@@ -2,6 +2,7 @@ package unit10;
 
 public class Recur {
 
+    // returns 3 to the power of n
     public static int fun3(int n) {
         if (n == 0)
             return 1;
@@ -9,6 +10,7 @@ public class Recur {
             return 3 * fun3(n - 1);
     }
 
+    //sierpinski triangle somthing somthing adding 1s and 0s somthing somthing...
     public static int f(int n) {
         if (n == 0)
             return 0;
@@ -18,12 +20,15 @@ public class Recur {
             return f(n - 1) + f(n - 2);
     }
 
+    //returns true if there are two matching characters next to each other in a string.
     public static boolean has(String input) {
         if (input.length() < 2)
             return false;
         return (input.charAt(0) == input.charAt(1) || has(input.substring(1)));
     }
 
+    //if the first two characters that are next to each other equal each other returns true.
+    //or length is less than 2.
     public static boolean pot(String input) {
         if (input.length() < 2)
             return true;
@@ -31,12 +36,14 @@ public class Recur {
                 && pot(input.substring(1, input.length() - 1)));
     }
 
+    //function A: 1 / (2 + A) within its self at A n times.
     public static double two(int n) {
         if (n < 1)
             return 1.0 / 2;
         return 1.0 / (2 + two(n - 1));
     }
 
+    // 
     public static String mop(int n) {
         if (n / 2 == 0)
             return "" + n % 2;
@@ -65,7 +72,18 @@ public class Recur {
         // System.out.println(fun3(5));
 
         // Consider function f
-        // System.out.println(f(6));
+         System.out.println(f(1));
+         System.out.println(f(2));
+         System.out.println(f(3));
+         System.out.println(f(4));
+         System.out.println(f(5));
+         System.out.println(f(6));
+         System.out.println(f(7));
+         System.out.println(f(8));
+         System.out.println(f(9));
+         System.out.println(f(10));
+         System.out.println(f(11));
+         System.out.println(f(12));
 
         // Consider function has
         // System.out.println(has("hello"));
